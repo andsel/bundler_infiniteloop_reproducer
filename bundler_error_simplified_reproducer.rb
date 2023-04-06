fa41015cfb719e504a2a0eda58a22bc6f3c47ac7
andsel <selva.andre@gmail.com>
2023-04-06 17:08:24 +0200

    Exposes inifite loop

---
+++ bundler_error_simplified_reproducer.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+# Script to test Bundler leak
+# 
+# To run, copy script to Logstash folder and run:
+# 
+# bin/ruby bundler_error_simplified_reproducer.rb
+
+require "bundler"
+
+cwd = ::File.expand_path(::File.join(__FILE__, ".."))
+puts "current working dir: #{cwd}"
+
+lock_file = Pathname.new(::File.join(cwd, "lockfiles", "Gemfile.lock"))
+puts "lock_file is: #{lock_file}"
+
+gemfile_path = ::File.join(cwd, "lockfiles", "logstash-input-cloudwatch")
+gemfile = ::File.new(gemfile_path, "r+").read
+puts "loaded content of #{gemfile_path}"
+
+builder = Bundler::Dsl.new
+builder.eval_gemfile("bundler file", gemfile)
+definition = builder.to_definition(lock_file, {})
+
+# the following is the incriminated
+puts "Before resolve_remotely!"
+definition.resolve_remotely!
